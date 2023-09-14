0b5ddec17bc413d6b00611f51c7cacb04c3e50d8
OmatrixTech <144843226+OmatrixTech@users.noreply.github.com>
2023-09-14 08:42:50 +0530

    Custom Loader
    A loader is a software component or program that is responsible for loading and preparing other software or data for execution or use by a computer system. Loaders are essential in the process of running programs and managing resources within a computing environment. 
    The specific functionality of a loader depends on its purpose and the context in which it operates. Loaders are essential components of computer systems because they facilitate the execution of programs and the management of resources required for those programs to run.

---
+++ WpfCustomLoaderTesting/WpfCustomLoaderTesting/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using WPF.CustomControls.LoadEase.Utility;
+
+namespace WpfCustomLoaderTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    //Creating a custom Wait Loader control in Windows Presentation Foundation(WPF) can enhance the user experience by providing visual feedback during time-consuming operations, such as data loading or processing.A Wait Loader is essentially an animated progress indicator that indicates to the user that something is happening in the background. It is a very simple solution which can be used to resolve WaitWindow issue in wpf.
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(20); // Adjust the interval as needed
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            LoaderHandler.ShowLoader();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //LoadingAdorner.IsAdornerVisible = false;
+            LoaderHandler.HideLoader();
+        }
+    }
+}
